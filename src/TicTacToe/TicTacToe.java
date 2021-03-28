018583b8368d037b8281c72aa341db0c7b376c1c
Eshaan Kansagara <eshaankansagara@gmail.com>
2021-03-27 21:46:29 -0700

    finished tic tac toe

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -4,93 +4,105 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-	private char[][] mat;
+    private char[][] mat;
 
-	private char letter;
-	private char[] gameArr;
+    private char letter;
+    private char[] gameArr;
 
-	private char letterWon;
+    private char letterWon;
 
-	public TicTacToe() {
+    public TicTacToe() {
 
-	}
+    }
 
-	public TicTacToe(String game) {
+    public TicTacToe(String game) {
 
-		loadGame(game);
+        loadGame(game);
 
-	}
+    }
 
-	public void loadGame(String game) {
+    public void loadGame(String game) {
 
-		gameArr = new char[game.length()];
+        gameArr = new char[game.length()];
 
-		for (int i = 0; i < game.length(); i++) {
+        for (int i = 0; i < game.length(); i++) {
 
-			letter = game.charAt(i);
+            letter = game.charAt(i);
 
-			gameArr[i] = letter;
+            gameArr[i] = letter;
 
-		}
+        }
 
-		// adding to 2d array
+        // adding to 2d array
 
-		mat = new char[3][3];
+        mat = new char[3][3];
+        int i = 0;
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat.length; col++) {
+                mat[row][col] = gameArr[i];
+                System.out.print(mat[row][col] + " ");
+                i++;
+            }
+            System.out.println();
 
-		int index = 0;
+        }
 
-		for (int r = 0; r < 3; r++) {
+    }
 
-			for (int c = 0; c < 3; c++) {
+    public String getWinner() {
 
-				if (index == gameArr.length) {
+        // check across
+        for (int row = 0; row < mat[0].length; row++) {
+            for (int col = 0; col < mat[0].length - 2; col++) {
+                if (mat[row][col] == mat[row][col + 1] && mat[row][col] == mat[row][col + 2] && mat[row][col] == 'X') {
+                    return "X wins horizontally";
+                }
+                if (mat[row][col] == mat[row][col + 1] && mat[row][col] == mat[row][col + 2] && mat[row][col] == 'O') {
+                    return "O wins horizontally";
+                }
+            }
+        }
 
-					break;
+        // check up and down
+        for (int row = 0; row < mat.length - 2; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (mat[row][col] == mat[row + 1][col] && mat[row][col] == mat[row + 2][col] && mat[row][col] == 'X') {
+                    return "X wins vertically";
+                }
+                if (mat[row][col] == mat[row + 1][col] && mat[row][col] == mat[row + 2][col] && mat[row][col] == 'O') {
+                    return "O wins vertically";
+                }
+            }
+        }
 
-				}
+        // check diagonally
+        for (int row = 2; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length - 2; col++) {
+                if (mat[row][col] == mat[row - 1][col + 1] && mat[row][col] == mat[row - 2][col + 2]
+                        && mat[row][col] == 'X') {
+                    return "X wins diagonally";
+                }
+                if (mat[row][col] == mat[row - 1][col + 1] && mat[row][col] == mat[row - 2][col + 2]
+                        && mat[row][col] == 'O') {
+                    return "O wins diagonally";
+                }
+            }
+        }
 
-				mat[r][c] = gameArr[r];
+        return "No winner";
 
-				System.out.print(mat[r][c] + " ");
+    }
 
-				index++;
+    public String toString() {
 
-			}
+        String output = "";
 
-			System.out.println();
+        output += getWinner();
 
-		}
+        // System.out.println(gameArr);
 
-	}
+        return output + "\n\n";
 
-	public String getWinner() {
-
-		boolean hasWon = false;
-
-		for (int r = 0; r < mat.length; r++) {
-
-			for (int c = 0; c < mat[0].length; c++) {
-
-			}
-
-		}
-
-		// return letterWon + " has won horizontally";
-
-		return "not won";
-
-	}
-
-	public String toString() {
-
-		String output = "";
-
-		output += getWinner();
-
-		// System.out.println(gameArr);
-
-		return output + "\n\n";
-
-	}
+    }
 
 }
